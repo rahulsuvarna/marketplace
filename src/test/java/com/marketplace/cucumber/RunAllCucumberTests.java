b019f4ae0a015df5975e36f275ff403d3beaecc2
dsrahul <dsrahul@yahoo.com>
2017-07-19 23:53:01 +0100

    Cucumber reports configuration

--- src/test/java/com/marketplace/cucumber/RunAllCucumberTests.java
+++ src/test/java/com/marketplace/cucumber/RunAllCucumberTests.java
@@ -5,7 +5,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(format = {"pretty", "html:target/cucumber"},
+@CucumberOptions(format = {"pretty", "json:target/cucumber.json"},
         features = "src/test/Features")
 
 public class RunAllCucumberTests {
